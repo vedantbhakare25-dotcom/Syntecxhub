72b7e6dd3fccb539465075a778ab752bf4ae5c09
vedantbhakare25-dotcom <vedant.bhakare25@vit.edu>
2025-12-15 21:47:25 +0530

    Initial commit: Syntecxhub projects

---
+++ Syntecxhub_DataExplorerAndAnalyzer/DataExplorerAndAnalyzer.py
@@ -0,0 +1,9 @@
+import numpy as np
+import time
+
+arr=np.array([10,20,30,40,50])
+matrix=np.arange(1,10).reshape(3,3)#
+
+
+print("First Element of Array:",arr[0])
+print("matrix Slice:\n",matrix[:2,:2])
