de40b04286a13776fc117b9a00e0de41471783d8
vedantbhakare25-dotcom <vedant.bhakare25@vit.edu>
2025-12-15 21:50:23 +0530

    updated

--- Syntecxhub_DataExplorerAndAnalyzer/DataExplorerAndAnalyzer.py
+++ Syntecxhub_DataExplorerAndAnalyzer/DataExplorerAndAnalyzer.py
@@ -7,3 +7,14 @@ matrix=np.arange(1,10).reshape(3,3)#
 
 print("First Element of Array:",arr[0])
 print("matrix Slice:\n",matrix[:2,:2])
+
+print("Addition:", arr + 5)
+print("Multiplication:", arr * 2)
+
+print("Mean:",np.mean(arr))
+print("Max:",np.max(arr))
+print("Standard Deviation:", np.std(arr))
+
+
+print("Column-wise sum:",np.sum(matrix, axis=0))
+print("Row-wise sum:",np.sum(matrix, axis=1))
