7236b2b66207c9dbaf2fe9fafb99a0b52e0a9fcf
vedantbhakare25-dotcom <vedant.bhakare25@vit.edu>
2025-12-15 21:52:43 +0530

    final update done

--- Syntecxhub_DataExplorerAndAnalyzer/DataExplorerAndAnalyzer.py
+++ Syntecxhub_DataExplorerAndAnalyzer/DataExplorerAndAnalyzer.py
@@ -8,13 +8,33 @@ matrix=np.arange(1,10).reshape(3,3)#
 print("First Element of Array:",arr[0])
 print("matrix Slice:\n",matrix[:2,:2])
 
-print("Addition:", arr + 5)
-print("Multiplication:", arr * 2)
+print("Addition:",arr + 5)
+print("Multiplication:",arr * 2)
 
 print("Mean:",np.mean(arr))
 print("Max:",np.max(arr))
-print("Standard Deviation:", np.std(arr))
+print("Standard Deviation:",np.std(arr))
 
 
-print("Column-wise sum:",np.sum(matrix, axis=0))
-print("Row-wise sum:",np.sum(matrix, axis=1))
+print("Column-wise sum:",np.sum(matrix,axis=0))
+print("Row-wise sum:",np.sum(matrix,axis=1))
+
+reshaped = arr.reshape(5,1)
+broadcasted = reshaped + np.array([1,2,3])
+print("Broadcasted array:\n",broadcasted)
+
+np.save("data.npy",arr)
+loaded_arr = np.load("data.npy")
+print("Loaded array:",loaded_arr)
+
+size = 1000000
+python_list = list(range(size))
+numpy_array = np.arange(size)
+
+start = time.time()
+[x * 2 for x in python_list]
+print("Python list time:",time.time() - start)
+
+start = time.time()
+numpy_array * 2
+print("NumPy array time:",time.time() - start)
